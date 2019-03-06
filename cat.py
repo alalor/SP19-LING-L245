c4227ca4d56e6984dc139bd99d34f046669fdc91
alalor <alalor@iu.edu>
2019-03-06 21:43:35 +0100

    new file

--- cat.py
+++ cat.py
@@ -1,5 +1,11 @@
+"""
+This is my program that acts like
+the Unix 'cat' command
+"""
 import sys
 
+# Read everythng from standard input
 text = sys.stdin.read()
 
+# Output everything to standard output
 sys.stdout.write(text)
